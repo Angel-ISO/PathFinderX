74c573fbf2ebe107bbf91257a98fc58d8bc660dd
Angel Gabriel <123566141+Angel-ISO@users.noreply.github.com>
2026-03-18 18:07:01 -0500

    Update CORS origin for production environment

--- backend/src/api/Extensions/CorsSetup.js
+++ backend/src/api/Extensions/CorsSetup.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 export function configureCors(app) {
   app.use(
     cors({
-      origin: ['http://localhost:5173','https://pathfinderx.stellarco.online', 'localhost:3000'],
+      origin: ['http://localhost:5173','https://pathfinderx-kyzp.onrender.com', 'localhost:3000'],
       methods: ['GET', 'POST', 'PUT','PATCH','DELETE'],
       allowedHeaders: ['Content-Type', 'Authorization'], 
       credentials: true,
